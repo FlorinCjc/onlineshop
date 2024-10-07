a7c4fad65a0f8c797360021d9ea53b8031af9ccd
FlorinCjc <florinncjc@gmail.com>
2024-10-07 19:16:00 +0100

    Toggle Registe-Login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,13 +7,14 @@ import {
   MatCardSubtitle,
   MatCardTitle
 } from "@angular/material/card";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
 import {MatInput} from "@angular/material/input";
 import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -33,30 +34,33 @@ import {AuthService} from "../services/auth.service";
     MatIconButton,
     MatInput,
     MatLabel,
-    MatSuffix
+    MatSuffix,
+    NgIf
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent { // ne am creat cele 4 proprietati urm pas e in html
+authType: string ="login";
+
 username: string="";
 email: string="";
 password: string="";
 confirmPassword: string="";
 
-
-constructor(private authService : AuthService) {
+// clasa Router ne ajuta sa navigam intre pagini in Angular
+constructor(private authService : AuthService, private  router: Router) {
   //am injectat authService
 }
 
 submitRegisterForm(){ //asa arata un JSON
   let payLoad = {
     username: this.username,
-    email: this.username,
+    email: this.email,
     password: this.password,
     confirmPassword: this.confirmPassword
   };
-  //TODO:
+
   this.authService.register(payLoad).subscribe((response : any) =>{
     console.log(response);
   });
@@ -71,6 +75,18 @@ submitLoginForm(){
   };
   this.authService.login(payLoad).subscribe((response : any) =>{
     console.log(response);
-  });
+    if(response.status == 200){
+      // daca conditia este adevarata inseamna ca ne-am logat cu succes
+    this.router.navigateByUrl("/dashboard");
+    }
+  },(errorResponse) => {{
+    console.log(errorResponse);
+    console.log(errorResponse.error);
+    alert(errorResponse.error.message);
+  }});
 }
+onAuthTypeChange(authType : string){
+  this.authType = authType;
+}
+
 }
